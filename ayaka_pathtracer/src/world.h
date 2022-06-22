4a5ca6f141f7cd8ec6c37e07ba462269b2e563c2
Hina Xayah <ycxiang14sep@gmail.com>
2022-06-22 19:28:13 +0800

    refine structures

--- ayaka_pathtracer/src/world.h
+++ ayaka_pathtracer/src/world.h
@@ -38,8 +38,8 @@ std::optional<hit_record> WorldT<Shape, Ray, hit_record>::intersect(const Ray &r
     auto closest_so_far = std::numeric_limits<float>::max();
     for (const auto &pair: _shapes)
     {
-        auto s = pair.second;
-        auto res = s->intersect(r._orig, r._dir, 0.1f, closest_so_far);
+        std::shared_ptr<Shape> s = pair.second;
+        std::optional<hit_record> res = s->intersect(r._orig, r._dir, 0.1f, closest_so_far);
         if (res.has_value())
         {
             closest_so_far = res.value().t;
